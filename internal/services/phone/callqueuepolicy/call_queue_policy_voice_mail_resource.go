925da4648726c2b159e440ce19e384b09246fac2
Tadayuki Onishi <tt.tanishi100@gmail.com>
2024-12-22 18:42:28 +0900

    fix zoom_phone_call_queue_policy_voice_mail resource logic (#74)
    Signed-off-by: Tadayuki Onishi <tonishi@folio-sec.com>
    Co-authored-by: Tadayuki Onishi <tonishi@folio-sec.com>

--- internal/services/phone/callqueuepolicy/call_queue_policy_voice_mail_resource.go
+++ internal/services/phone/callqueuepolicy/call_queue_policy_voice_mail_resource.go
@@ -3,11 +3,10 @@ package callqueuepolicy
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-framework/path"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
-	"github.com/samber/lo"
 	"strings"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/samber/lo"
 
 	"github.com/folio-sec/terraform-provider-zoom/internal/provider/shared"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
@@ -89,8 +88,7 @@ This resource requires the ` + strings.Join([]string{
 						},
 						"shared_id": schema.StringAttribute{
 							Computed:            true,
-							MarkdownDescription: "The number is limited to the minimum value of 10 or the number of allowed access members account setting.",
-							PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
+							MarkdownDescription: "The shared ID of the voicemail access member.",
 						},
 					},
 				},
